6cb6a2918b0b1d8da5937d9ce208710b0a83f9d3
EngineerBoye <deusiratus001@gmail.com>
2023-01-14 13:02:39 +0100

    Dodaj pliki projektów.

---
+++ View Model/Database_View_Model.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using T_Komp_zadanie.Model;
+
+namespace T_Komp_zadanie.View_Model
+{
+    public class Database_View_Model 
+    {
+        private ICommand m_ButtonCommand;
+        public ICommand ButtonCommand //komenda do której bindujemy przycisk
+        {
+            get
+            {
+                return m_ButtonCommand;
+            }
+            set
+            {
+                m_ButtonCommand = value;
+            }
+        }
+        public Database_View_Model()
+        {
+            ButtonCommand = new RelayCommand(new Action<object>(_LoginButtonClick));
+        }
+        private void _LoginButtonClick(object sender)
+        {
+           
+        }
+    }
+}
---
+++ View/Database_View.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using T_Komp_zadanie.View_Model;
+
+namespace T_Komp_zadanie.View
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Database_View.xaml
+    /// </summary>
+    public partial class Database_View : Window
+    {
+        public Database_View()
+        {
+            InitializeComponent();
+            this.DataContext = new Database_View_Model(); //Powiązanie view z view model
+        }
+
+    }
+}
